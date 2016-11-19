b7e57c5ba917b7bb2a054ab8d1fba11721d28618
Kellie Spears <kelliespears@gmail.com>
2016-11-19 07:11:35 -0800

    setting up ViewController/ViewControllerTest

---
+++ SieveOfEratosthenesTests/ViewControllerTest.m
@@ -0,0 +1,44 @@
+//
+//  ViewControllerTest.m
+//  SieveOfEratosthenes
+//
+//  Created by Kellie Spears on 11/19/16.
+//  Copyright © 2016 Kellie Spears. All rights reserved.
+//
+
+// Collaborators
+#import <XCTest/XCTest.h>
+#import <OCHamcrest.h>
+
+// Class under test
+#import "ViewController.h"
+
+@interface ViewControllerTest : XCTestCase
+
+@end
+
+@implementation ViewControllerTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
